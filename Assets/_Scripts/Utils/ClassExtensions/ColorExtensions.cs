c8e7b1583991d427de2420b3cd5f01a90fbef6a3
Gebruiker <ik_ben_ramses@hotmail.com>
2016-04-24 16:30:03 +0200

    Wrote extensions for both the Color and Image class to make it easier to change the alpha channel.

---
+++ Assets/_Scripts/Utils/ClassExtensions/ColorExtensions.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ColorExtensions
+{
+	public static Color AlphaVersion(this Color color, float alpha)
+	{
+		Color c = color;
+		return new Color(c.r, c.g, c.b, alpha);
+	}
+}
---
+++ Assets/_Scripts/Utils/ClassExtensions/ImageExtensions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public static class ImageExtensions {
+
+	public static Color SetAlpha(this Image image, float alpha)
+	{
+		image.color = image.color.AlphaVersion(alpha);
+		return image.color;
+	}
+}
