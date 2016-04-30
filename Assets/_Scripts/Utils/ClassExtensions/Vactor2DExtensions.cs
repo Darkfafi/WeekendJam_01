7cc646c63bf32ea715ab027061eecab6a030637f
Gebruiker <ik_ben_ramses@hotmail.com>
2016-04-30 13:40:47 +0200

    Added Vector2D Extensions

---
+++ Assets/_Scripts/Utils/ClassExtensions/Vactor2DExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Vactor2DExtensions {
+
+	public static Vector2 Vec3Add(this Vector2 vec2D,Vector3 vec3D, bool zIsY = false)
+	{
+		Vector2 adder = new Vector2(vec3D.x,(zIsY) ? vec3D.z : vec3D.y);
+		return vec2D += adder;
+    }
+	public static Vector2 Vec3Substract(this Vector2 vec2D, Vector3 vec3D, bool zIsY = false)
+	{
+		Vector2 sub = new Vector2(vec3D.x, (zIsY) ? vec3D.z : vec3D.y);
+		return vec2D -= sub;
+	}
+}
