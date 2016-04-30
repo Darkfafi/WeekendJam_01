2e958948c9910f21f9a601b0b23d8a358e65b2fc
Gebruiker <ik_ben_ramses@hotmail.com>
2016-04-30 14:55:18 +0200

    Fixed calculation to get all interestpoints into screen without any of them sticking out. (Dynamic camera)

--- Assets/_Scripts/Camera/CameraBoundFocus.cs
+++ Assets/_Scripts/Camera/CameraBoundFocus.cs
@@ -46,20 +46,45 @@ public class CameraBoundFocus : MonoBehaviour
 
 		if (entities.Length > 0)
 		{
-			Vector2 centerOfObjects = entities[0].Position;
+			Vector2 centerOfObjects = new Vector2(0,0);
 			Vector2 diff;
+			Vector2 startCamCalcPos = entities[0].Position;
+
 			float xDist = entities[0].Size.x;
 			float yDist = entities[0].Size.y;
+            float mostLeft = entities[0].Position.x;
+			float mostRight = entities[0].Position.x;
+			float mostTop = entities[0].Position.y;
+			float mostDown = entities[0].Position.y;
 
 			for (int i = entities.Length - 1; i >= 1; i--)
 			{
-				diff = entities[i].Position - centerOfObjects;
-                centerOfObjects += (diff * 0.5f);
-            }
-
+				if (mostLeft > entities[i].Position.x)
+				{
+					mostLeft = entities[i].Position.x;
+					startCamCalcPos = entities[i].Position;
+                }
+				if (mostRight < entities[i].Position.x)
+				{
+					mostRight = entities[i].Position.x;
+				}
+				if (mostTop < entities[i].Position.y)
+				{
+					mostTop = entities[i].Position.y;
+				}
+				if (mostDown > entities[i].Position.y)
+				{
+					mostDown = entities[i].Position.y;
+				}
+			}
+			float x = mostRight - mostLeft;
+			float y = mostTop - mostDown;
+            x *= 0.5f;
+			y *= 0.5f;
+			centerOfObjects = startCamCalcPos + new Vector2(x, y);
 			centerOfObjects += offset;
 
-			Debug.DrawLine(entities[0].Position, centerOfObjects);
+			Debug.DrawLine(startCamCalcPos, centerOfObjects);
 
 			for (int i = entities.Length - 1; i >= 0; i--)
 			{
@@ -77,7 +102,7 @@ public class CameraBoundFocus : MonoBehaviour
 			centerOfObjects.x = Mathf.Clamp(centerOfObjects.x, Left, Right);
 			centerOfObjects.y = Mathf.Clamp(centerOfObjects.y, Bottom, Top);
 
-			Debug.DrawLine(entities[0].Position, centerOfObjects, Color.red);
+			Debug.DrawLine(startCamCalcPos, centerOfObjects, Color.red);
 
 			Vector2 lerpedCamPos = Vector2.Lerp(thisCamera.transform.position, centerOfObjects, Time.deltaTime * lerpPositionSpeed);
             thisCamera.transform.position = new Vector3(Mathf.Clamp(lerpedCamPos.x, Left, Right), Mathf.Clamp(lerpedCamPos.y, Bottom, Top), - 10);
